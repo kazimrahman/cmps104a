be2313a909902d1249c25a1f292db945acf01651
Steven Dariush Morad <smorad@unix1.lt.ucsc.edu>
2014-12-12 12:28:48 -0800

    I fucked it up, rolling back to  6396582788261ad74217aff7dcacc3b339c4eeb0

--- asg5/emit.cc
+++ asg5/emit.cc
@@ -10,6 +10,7 @@ size_t ind_ptr_count = 0;
 symbol_table* symtable = new symbol_table;
 void emit_node(FILE* file, astree* node);
 void emit_expr(FILE* file, astree* node);
+void emit_new_vreg(FILE* file, astree* node);
 
 void indent(FILE* file){
    fprintf(file, "        ");
@@ -148,119 +149,166 @@ void emit_ifelse(FILE* file, astree* node){
 
 }
 
-void emit_expr(FILE* file, astree* node){
-   indent(file);
-   symbol* sym = nullptr;
-   char* type_buf = nullptr;
-   int bval = 0;
-   switch(node->symbol){
-      case TOK_FALSE:
-      case TOK_TRUE:
-         node->vreg = new_vreg('c');
-         type_buf = strdup("char");
-         if(node->symbol == TOK_TRUE)
-            bval = 1;
-         fprintf(file, "%s %s = %d;\n", 
-            type_buf,
-            node->vreg.c_str(), bval);
-         st_insert(symtable, node);
-         break;
-      case TOK_IDENT:
-         sym = st_lookup(symtable, node);
-         if(sym != nullptr){
-            fprintf(file, " %s ", 
-               node->vreg.c_str());
-            break;
-         }
-         //fall thru to decl fprintf if undecl
-      case TOK_INTCON:
-      case TOK_CHARCON:
-         if(node->attr[attr_bool] || node->attr[attr_char]){
-            node->vreg = new_vreg('c');
-            type_buf = strdup("char");
-         }
-         else if(node->attr[attr_int]){
-            node->vreg = new_vreg('i');
-            type_buf = strdup("int");
-         }
-         else if(node->attr[attr_struct]){
-            node->vreg = new_vreg('p');
-            type_buf = (char*) node->children[0]->lexinfo->c_str();
-         }
-         else{
-            fprintf(file, "%s;\n", node->lexinfo->c_str());
-            break;
-         }
-         fprintf(file, "%s %s = %s;\n", 
-            type_buf,
-            node->vreg.c_str(), node->lexinfo->c_str());
-         st_insert(symtable, node);
-         break;
-      case '+':
-      case '-':
-      case '/':
-      case '*':
-      case '%':
-      case TOK_EQ:
-      case TOK_NE:
-      case TOK_LT:
-      case TOK_LE:
-      case TOK_GT:
-      case TOK_GE:
-         if(node->attr[attr_bool] || node->attr[attr_char]){
-            node->vreg = new_vreg('c');
-            type_buf = strdup("char");
-         }
-         else if(node->attr[attr_int]){
-            node->vreg = new_vreg('i');
-            type_buf = strdup("int");
-         }
-         else if(node->attr[attr_struct]){
-            node->vreg = new_vreg('p');
-            type_buf = (char*) node->children[0]->lexinfo->c_str();
-         }
-         //for 90-c8q.oc
-         if(node->children.empty() || 
-               node->children[0] == nullptr || 
-               node->children[1] == nullptr)
-            break;
-         fprintf(file, "%s %s = %s %s %s;\n", 
-            type_buf,
-            node->vreg.c_str(),
+//void emit_expr(FILE* file, astree* node){
+//   symbol* sym = nullptr;
+//   char* type_buf = nullptr;
+//   int bval = 0;
+//   switch(node->symbol){
+//      case TOK_FALSE:
+//      case TOK_TRUE:
+//         node->vreg = new_vreg('c');
+//         type_buf = strdup("char");
+//         if(node->symbol == TOK_TRUE)
+//            bval = 1;
+//         fprintf(file, "%s %s = %d;\n", 
+//            type_buf,
+//            node->vreg.c_str(), bval);
+//         st_insert(symtable, node);
+//         break;
+//      case TOK_IDENT:
+//         sym = st_lookup(symtable, node);
+//         if(sym != nullptr){
+//            fprintf(file, " %s ", 
+//               node->vreg.c_str());
+//            break;
+//         }
+//         //fall thru to decl fprintf if undecl
+//      case TOK_INTCON:
+//      case TOK_CHARCON:
+//         if(node->attr[attr_bool] || node->attr[attr_char]){
+//            node->vreg = new_vreg('c');
+//            type_buf = strdup("char");
+//         }
+//         else if(node->attr[attr_int]){
+//            node->vreg = new_vreg('i');
+//            type_buf = strdup("int");
+//         }
+//         else if(node->attr[attr_struct]){
+//            node->vreg = new_vreg('p');
+//            type_buf = (char*) node->children[0]->lexinfo->c_str();
+//         }
+//         else{
+//            fprintf(file, "%s;\n", node->lexinfo->c_str());
+//            break;
+//         }
+//         fprintf(file, "%s %s = %s;\n", 
+//            type_buf,
+//            node->vreg.c_str(), node->lexinfo->c_str());
+//         st_insert(symtable, node);
+//         break;
+//      case '+':
+//      case '-':
+//      case '/':
+//      case '*':
+//      case '%':
+//      case TOK_EQ:
+//      case TOK_NE:
+//      case TOK_LT:
+//      case TOK_LE:
+//      case TOK_GT:
+//      case TOK_GE:
+//         if(node->attr[attr_bool] || node->attr[attr_char]){
+//            node->vreg = new_vreg('c');
+//            type_buf = strdup("char");
+//         }
+//         else if(node->attr[attr_int]){
+//            node->vreg = new_vreg('i');
+//            type_buf = strdup("int");
+//         }
+//         else if(node->attr[attr_struct]){
+//            node->vreg = new_vreg('p');
+//            type_buf = (char*) node->children[0]->lexinfo->c_str();
+//         }
+//         //for 90-c8q.oc
+//         if(node->children.empty() || 
+//               node->children[0] == nullptr || 
+//               node->children[1] == nullptr)
+//            break;
+//         fprintf(file, "%s %s = %s %s %s;\n", 
+//            type_buf,
+//            node->vreg.c_str(),
+//            node->children[0]->vreg.c_str(),
+//            node->lexinfo->c_str(),
+//            node->children[1]->vreg.c_str()); 
+//         break;
+//      case TOK_ORD:
+//         node->vreg = new_vreg('i');
+//         type_buf = strdup("int");
+//         node->vreg = new_vreg('c');
+//         type_buf = strdup("char");
+//         fprintf(file, "%s %s = %s;\n", 
+//            type_buf,
+//            node->vreg.c_str(), node->lexinfo->c_str());
+//         st_insert(symtable, node);
+//         break;
+//      case TOK_CHR:
+//         node->vreg = new_vreg('c');
+//         type_buf = strdup("char");
+//         fprintf(file, "%s %s = %s;\n", 
+//            type_buf,
+//            node->vreg.c_str(), node->lexinfo->c_str());
+//         st_insert(symtable, node);
+//         break;
+//      case TOK_NEG:
+//         node->vreg = new_vreg('i');
+//         type_buf = strdup("int");
+//         fprintf(file, "%s %s = %s;\n", 
+//            type_buf,
+//            node->vreg.c_str(), node->lexinfo->c_str());
+//         st_insert(symtable, node);
+//         break;
+//         
+//         default:
+//            errprintf("unkown expression: %s", node->lexinfo->c_str());
+//   }
+//}
+
+void emit_binop(FILE* file, astree* node){
+   //emits in the form: a * b
+   //if it exist, don't redecl
+   symbol* sym = st_lookup(symtable, node);
+   string vreg;
+   if(sym != nullptr){
+      vreg = sym->vreg;
+      fprintf(file, "%s %s %s", 
             node->children[0]->vreg.c_str(),
             node->lexinfo->c_str(),
-            node->children[1]->vreg.c_str()); 
-         break;
-      case TOK_ORD:
-         node->vreg = new_vreg('i');
-         type_buf = strdup("int");
-         node->vreg = new_vreg('c');
-         type_buf = strdup("char");
-         fprintf(file, "%s %s = %s;\n", 
-            type_buf,
-            node->vreg.c_str(), node->lexinfo->c_str());
-         st_insert(symtable, node);
-         break;
-      case TOK_CHR:
-         node->vreg = new_vreg('c');
-         type_buf = strdup("char");
-         fprintf(file, "%s %s = %s;\n", 
-            type_buf,
-            node->vreg.c_str(), node->lexinfo->c_str());
-         st_insert(symtable, node);
-         break;
-      case TOK_NEG:
-         node->vreg = new_vreg('i');
-         type_buf = strdup("int");
-         fprintf(file, "%s %s = %s;\n", 
-            type_buf,
-            node->vreg.c_str(), node->lexinfo->c_str());
-         st_insert(symtable, node);
-         break;
-         
-         default:
-            errprintf("unkown expression: %s", node->lexinfo->c_str());
+            node->children[1]->vreg.c_str()
+            );
    }
+   else{
+      //emit_new_vreg(file, node);
+      fprintf(file, "%s %s %s",
+            node->children[0]->vreg.c_str(),
+            node->lexinfo->c_str(),
+            node->children[1]->vreg.c_str()
+            );
+   }
+}
+
+void emit_ident(FILE* file, astree* node){
+   printf("Emit ident: %s\n", node->lexinfo->c_str());
+   string ident = mangle_ident(node);
+   fprintf(file, ident.c_str());
+   node->vreg = ident;
+}
+
+void emit_expr(FILE* file, astree* node){
+   if(node->attr[attr_vreg]){
+      emit_binop(file, node);
+   }
+   else{
+      switch(node->symbol){
+         case TOK_IDENT:
+            emit_ident(file, node);
+            break;
+         case TOK_INTCON:
+         case TOK_CHARCON:
+            emit_new_vreg(file, node);
+            break;
+      }
+   }
+   fprintf(file, "\n");
 }
 
 void emit_rec(FILE* file, astree* node){
@@ -280,6 +328,82 @@ void emit_return(FILE* file, astree* node){
    }
 }
 
+void emit_assignment(FILE* file, astree* node){
+   symbol* sym = st_lookup(symtable, node);
+   //if sym is null declare it
+   char* type_buf = nullptr;
+   indent(file);
+   if(node->attr[attr_bool] || node->attr[attr_char]){
+      node->vreg = new_vreg('c');
+      type_buf = strdup("char");
+   }
+   else if(node->attr[attr_int]){
+      node->vreg = new_vreg('i');
+      type_buf = strdup("int");
+   }
+   else if(node->attr[attr_struct]){
+      node->vreg = new_vreg('p');
+      type_buf = (char*) node->children[0]->lexinfo->c_str();
+   }
+   if(sym == nullptr){
+      fprintf(file, "%s %s %s %s;\n",
+            type_buf, node->children[0]->vreg.c_str(),
+            node->lexinfo->c_str(), node->vreg.c_str());
+   }
+   else{
+      fprintf(file, "%s %s %s;\n",
+            node->children[0]->vreg.c_str(),
+            node->lexinfo->c_str(),
+            node->vreg.c_str());
+   }
+}
+
+void emit_new_vreg(FILE* file, astree* node){
+   //emits in the form: int i4
+   char* type_buf;
+   if(node->attr[attr_bool] || node->attr[attr_char]){
+      node->vreg = new_vreg('c');
+      type_buf = strdup("char");
+   }
+   else if(node->attr[attr_int]){
+      node->vreg = new_vreg('i');
+      type_buf = strdup("int");
+   }
+   else if(node->attr[attr_struct]){
+      node->vreg = new_vreg('p');
+      type_buf = (char*) node->children[0]->lexinfo->c_str();
+   }
+   else{
+      node->vreg = new_vreg('a');
+      type_buf = strdup("unknown");
+   }
+   fprintf(file, "%s %s", 
+      type_buf,
+      node->vreg.c_str());
+   st_insert(symtable, node);
+
+}
+
+void emit_call(FILE* file, astree* node){
+   //emits in the form i1 = int func(param, param);
+   if(!node->attr[attr_void]){
+      //return type & assignment
+      emit_new_vreg(file, node);
+   }
+      fprintf(file, "%s ( ", 
+            node->children.back()->lexinfo->c_str());
+      //print params
+      for(auto child = node->children.begin(); 
+            child != node->children.end()-1; child++){
+            fprintf(file, "%s", (*child)->vreg.c_str());
+            if(child != node->children.end()-2)
+               fprintf(file, ", ");
+            else
+               fprintf(file, " );\n");
+      }
+
+}
+
 void emit_node(FILE* file, astree* node){
    switch(node->symbol){
       case TOK_WHILE:
@@ -292,10 +416,14 @@ void emit_node(FILE* file, astree* node){
          emit_ifelse(file, node);
          break;
       case TOK_VARDECL:
+         emit_assignment(file, node);
          break;
       case TOK_RETURN:
          emit_return(file, node); 
          break; 
+      case TOK_CALL:
+         emit_call(file, node);
+         break;
 
 
       case '+':
@@ -318,16 +446,13 @@ void emit_node(FILE* file, astree* node){
    }
 }
 
-void swap_bool_char(astree* root){
-   for(auto child : root->children)
-      swap_bool_char(child);
-   if(root->symbol == TOK_BOOL)
-      root->symbol = TOK_CHAR;
-}
-
 void emit_stringcon(FILE* file, astree* node){
+   string vreg = new_vreg('s');
+   node->vreg = vreg;
    fprintf(file, "char* %s = %s;\n", 
-      new_vreg('s').c_str(), node->lexinfo->c_str());
+      vreg.c_str(), node->lexinfo->c_str());
+   st_insert(symtable, node);
+
 }
 
 string mangle_struct(astree* node){
@@ -350,7 +475,11 @@ string mangle_struct(astree* node){
 
 string mangle_ident(astree* node){
    string builder = *node->lexinfo + " ";
-   astree* child = node->children[0];
+   astree* child = nullptr;
+   if (!node->children.empty())
+      child = node->children[0];
+   else 
+      child = node;
    //global
    if (node->blocknr == 0){
       builder += "__";
@@ -397,7 +526,6 @@ void emit_func(FILE* file, astree* node){
 
 void emit_oil(FILE* file, astree* root){
    //change all bool to char
-//   swap_bool_char(root);
    //first structs
    for(auto child : root->children){
       if(child->symbol == TOK_STRUCT){
@@ -415,11 +543,14 @@ void emit_oil(FILE* file, astree* root){
    for(auto child : root->children){
       if(child->symbol == TOK_VARDECL){
          fprintf(file, "        ");
-         fprintf(file, mangle_ident(child->children[0]).c_str());
+         child->vreg = mangle_ident(child->children[0]);
+         fprintf(file, child->vreg.c_str());
+      }
+      else if (child->symbol != TOK_FUNCTION && child->symbol != TOK_PROTOTYPE){
+         //rest of expr and stmt in global scope
+         emit_rec(file, root);
       }
    }
-   //rest of expr and stmt in global scope
-   emit_rec(file, root);
    fprintf(file, "}\n");
    //funcs
    for(auto child : root->children){
